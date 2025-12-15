a3b11c409308b9342ea25a9883b6bd138b92b132
Liad Oz <ozliad10@gmail.com>
2025-12-15 16:47:48 +0200

    Create server_config.json, refactor server const and database management, remove unused directories

--- src/server/server_const.py
+++ src/server/server_const.py
@@ -3,8 +3,6 @@ server_const.py
 Constants for authentication server configuration and responses.
 """
 
-import defenses as defenses_const
-
 # Response statues
 SERVER_SUCCESS = "success"
 SERVER_FAILURE = "fail"
@@ -14,11 +12,6 @@ SERVER_MSG_REGISTER_OK = "User registered successfully"
 SERVER_MSG_REGISTER_UNIQUE_FAIL = "Username already exist"
 SERVER_MSG_LOGIN_OK = "User login successfully"
 SERVER_MSG_LOGIN_INVALID = "Invalid credentials"
-
-# Defaults
-DEFAULT_HOST = "0.0.0.0"
-DEFAULT_PORT = 8000
-DEFAULT_HASH_MODE = defenses_const.HASH_SHA256
 
 # Paths
 DB_PATH = "src/server/db/auth.db"
