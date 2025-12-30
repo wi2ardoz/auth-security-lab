7051e4af5c2d526e12b02b12fab8ff8ea28683de
Liad Oz <ozliad10@gmail.com>
2025-12-30 16:19:32 +0200

    Update defenses package

--- src/server/defenses/__init__.py
+++ src/server/defenses/__init__.py
@@ -5,10 +5,12 @@ Defenses package initialization.
 
 from .defenses_const import HASH_ARGON2ID, HASH_BCRYPT, HASH_SHA256
 from .hash import hash_password, verify_password
+from .rate_limiter import check_rate_limit
 
 __all__ = [
     "hash_password",
     "verify_password",
+    "check_rate_limit",
     "HASH_SHA256",
     "HASH_BCRYPT",
     "HASH_ARGON2ID",
